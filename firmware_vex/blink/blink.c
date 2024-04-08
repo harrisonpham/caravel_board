d89ffc613abb8128f023931856cbcc9fe49797df
Harrison Pham <harrison@harrisonpham.com>
2024-04-07 18:20:46 -0700

    Working blinky

--- firmware_vex/blink/blink.c
+++ firmware_vex/blink/blink.c
@@ -139,53 +139,55 @@ void main()
     reg_gpio_ien = 1;
     reg_gpio_oe = 1;
 
-    reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_35 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_34 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_31 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_30 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_29 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_28 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_27 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_26 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_25 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_24 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_23 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_22 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_21 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_20 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_19 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_18 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_17 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_16 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_15 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_14 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_13 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_12 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_11 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_10 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_9  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_8  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-    reg_mprj_io_7  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_35 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_34 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
+//     reg_mprj_io_31 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_30 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_29 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_28 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_27 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_26 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_25 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_24 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_23 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_22 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_21 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_20 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_19 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_18 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_17 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_16 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_15 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_14 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_13 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_12 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_11 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_10 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_9  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_8  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+//     reg_mprj_io_7  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
     reg_mprj_io_6  = GPIO_MODE_MGMT_STD_OUTPUT;
-//    reg_mprj_io_6  = 0x1fff;
+// //    reg_mprj_io_6  = 0x1fff;
+//     reg_mprj_io_5  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+
+//     /* Lowest 5 GPIOs need to be set this way in order to	*/
+//     /* access the housekeeping SPI at run-time.  Do not change	*/
+//     /* them unless absolutely necessary.			*/
+
     reg_mprj_io_5  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
-
-    /* Lowest 5 GPIOs need to be set this way in order to	*/
-    /* access the housekeeping SPI at run-time.  Do not change	*/
-    /* them unless absolutely necessary.			*/
-
     reg_mprj_io_4  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
     reg_mprj_io_3  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
     reg_mprj_io_2  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
     reg_mprj_io_1  = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_0  = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
 
     // Initiate the serial transfer
-    reg_mprj_xfer = 1;
-    while (reg_mprj_xfer == 1);
+    // reg_mprj_xfer = 1;
+    // while (reg_mprj_xfer == 1);
 //
 //    /* GPIO 0 is turned off to prevent toggling the debug pin;	*/
 //    /* For debug, make this an output and drive it externally	*/
@@ -196,24 +198,26 @@ void main()
 //    gpio_program();
 //    gpio_program_local();
 
-//    reg_uart_enable = 1;
+   reg_uart_enable = 1;
 
 //    print("Hello World !!");
 //    putchar('x');
 
 	while (1) {
 
-        reg_gpio_out = 1; // OFF
-        reg_mprj_datal = 0x00000000;
-        reg_mprj_datah = 0x00000000;
+        // print("Hello World !!\n");
 
-		delay(8000000);
+        reg_gpio_out = 1; // OFF
+        // reg_mprj_datal = 0x00000000;
+        // reg_mprj_datah = 0x00000000;
+
+		delay(10000000);
 
         reg_gpio_out = 0;  // ON
-        reg_mprj_datah = 0x0000003f;
-        reg_mprj_datal = 0xffffffff;
+        // reg_mprj_datah = 0x0000003f;
+        // reg_mprj_datal = 0xffffffff;
 
-		delay(8000000);
+		delay(10000000);
 
     }
 
